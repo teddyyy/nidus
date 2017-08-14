7027add432c28f5c161874d62074d334d518e35e
teddy <teddy@sfc.wide.ad.jp>
2017-08-14 20:42:26 +0900

    add encoding declaration

--- slack.py
+++ slack.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from slackclient import SlackClient
 
 class Slack:
+
     def __init__(self, token):
         self.client = SlackClient(token)
 
     def post(self, channel, text, username):
         self.client.api_call("chat.postMessage",
-                                channel=channel,
-                                text=text,
-                                username=username)
+                             channel=channel,
+                             text=text,
+                             username=username)
